07c7a7ad88fb314ba3d0a06d891bcc6804e91f00
SISODIA2303 <sisodia23.siddharth@gmail.com>
2026-04-04 18:35:16 +0530

    fix server entry point with main() function

--- server/app.py
+++ server/app.py
@@ -1,1 +1,13 @@
+import uvicorn
 from app import app
+
+def main():
+    uvicorn.run(
+        "app:app",
+        host="0.0.0",
+        port=7860,
+        reload=False,
+    )
+
+if __name__ == "__main__":
+    main()
